b80b9b6fa1bebac5d370c91ceb569caf9881459e
YUAN Ji <46006577+dylan4932@users.noreply.github.com>
2021-07-26 17:16:19 +0800

    july 26th updates
    leetcode problem#9 & #14

--- Python/july.py
+++ Python/july.py
@@ -214,6 +214,83 @@ def longestPalindrome(s):
             r += 1
     return res
 
+##############July 26th
+def romanToInt(s):
+    '''
+    Roman numerals are represented by seven different symbols:
+    I, V, X, L, C, D and M.
+
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+    For example, 2 is written as II in Roman numeral,
+    just two one's added together. 12 is written as XII,
+    which is simply X + II. The number 27 is written as XXVII,
+    which is XX + V + II.
+
+    Roman numerals are usually written largest to smallest from
+    left to right. However, the numeral for four is not IIII.
+    Instead, the number four is written as IV. Because the one
+    is before the five we subtract it making four. The same principle
+    applies to the number nine, which is written as IX. There are six
+    instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+    Given a roman numeral, convert it to an integer.
+    
+    >>> romanToInt('III')
+    3
+    >>> romanToInt('IV')
+    4
+    >>> romanToInt('IX')
+    9
+    >>> romanToInt('LVIII')
+    58
+    >>> romanToInt('MCMXCIV')
+    1994
+    '''
+    dict0 = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    list0 = []
+    list0[:0] = s
+    list0.append('I')
+    ans = 0
+    for i in range(len(list0)-1):
+        if dict0[list0[i]] >= dict0[list0[i+1]]:
+            ans += dict0[list0[i]]
+        else:
+            ans -= dict0[list0[i]]
+    return ans
+
+def longestCommonPrefix(strs):
+    '''
+    Write a function to find the longest common prefix string amongst
+    an array of strings.
+
+    If there is no common prefix, return an empty string "".
+
+    >>> longestCommonPrefix(['flow', 'flower', 'flight'])
+    'fl'
+    >>> longestCommonPrefix(["dog","racecar","car"])
+    ''
+    >>> longestCommonPrefix(['sunshine', 'sunflower', 'sunroof'])
+    'sun'
+    '''
+    res = ''
+    cur = sorted(strs)
+    for i in cur[0]:
+        if cur[-1].startswith(res+i):
+            res += i
+        else:
+            break
+    return res
+
 # DocString Test
 doctest.testmod()
 
